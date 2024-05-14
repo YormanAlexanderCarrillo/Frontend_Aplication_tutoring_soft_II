0d869ec18bf5f6f0bb65103c2455d1d8d4eac966
DanielRivas18 <jose.rivas@uptc.edu.co>
2024-05-14 12:03:06 -0500

    modified:   se agrego la barra de navegacion     para el usuario

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,101 @@
+"use client"
+import { signOut } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function NavBar() {
+
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const menuItems = [
+
+    { text: "Datos personales", link: "/dashboard" },
+    { text: "Agendar", link: "/schedule" },
+    { text: "Material de apoyo", link: "/support" },
+    { text: "Foros", link: "/forum" }
+  ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleSignOut = () => {
+    setIsLoading(true)
+    signOut();
+    setIsLoading(false)
+    router.push("/login");
+  }
+
   return (
-    <div>
-      <nav className="bg-zinc-800/85 ">
-        <div className="flex items-center justify-center space-x-10 h-16">
-          <Link
-            href="/"
-            className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
-          >
-            Inicio
-          </Link>
-          <Link
-            href="/login"
-            className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
-          >
-            Ingresar
-          </Link>
-          <Link
-            href="/register"
-            className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
-          >
-            Registrarse
-          </Link>
-        </div>
-      </nav>
+    <div className="fixed top-0 w-full z-10 shadow-md">
+      {session?.user ? (
+        <Navbar>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Cerrar" : "Abrir"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <p className="font-bold text-inherit">UPTC</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            {menuItems.map((item, index) => (
+              <NavbarItem key={index}>
+                <Link color="foreground" href={item.link} onClick={toggleMenu}>
+                  {item.text}
+                </Link>
+              </NavbarItem>
+            ))}
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem>
+              <Button color="danger" variant="flat" onClick={handleSignOut} isLoading={isLoading}>
+                Cerrar Sesión
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarMenu >
+            {menuItems.map((item, index) => (
+              <NavbarMenuItem key={index}>
+                <Link
+                  color="foreground"
+                  href={item.link}
+                  onClick={() => toggleMenu}
+                >
+                  {item.text}
+                </Link>
+              </NavbarMenuItem>
+            ))}
+          </NavbarMenu>
+        </Navbar>
+      ) : (
+
+        <nav className="bg-zinc-800/85 ">
+          <div className="flex items-center justify-center space-x-10 h-16">
+            <Link
+              href="/"
+              className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
+            >
+              Inicio
+            </Link>
+            <Link
+              href="/login"
+              className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
+            >
+              Ingresar
+            </Link>
+            <Link
+              href="/register"
+              className="w-32 h-7 hover:bg-slate-200/20 text-white rounded-md flex justify-center items-center"
+            >
+              Registrarse
+            </Link>
+          </div>
+        </nav>
+      )}
     </div>
   );
 }
